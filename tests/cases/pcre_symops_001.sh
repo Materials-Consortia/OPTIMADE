9a75f6a5e76a4d6202021bc0d3b11ac2a4700fca
Saulius Gražulis <grazulis@ibt.lt>
2023-06-06 09:42:57 +0300

    Adding a test for the symmetry operator PCER.

---
+++ tests/cases/pcre_symops_001.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# Test case: test if a provided PCRE correctly recognises integer and
+# real (floating-point) numbers.
+
+#BEGIN DEPEND
+
+INPUT_DEFS=tests/generated/symop_definitions.pcre
+INPUT_GRAMMAR=tests/generated/symops.pcre
+
+#END DEPEND
+
+perl -I. -w -ne "
+     require '${INPUT_DEFS}';
+     print if m:$(grep -v '^ *#' ${INPUT_GRAMMAR} | perl -pe 's/ #.*//'):x
+" \
+     tests/inputs/symops.lst
