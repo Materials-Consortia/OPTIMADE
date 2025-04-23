56d1dc54bc775ae8e264ddb52206d4e3365beaf2
saulius <saulius>
2025-04-23 14:08:40 +0300

    Adding comments to the ".dir-locals.el" Emacs setting file.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,11 @@
+;;
+;; This file sets editng style for Emacs which is approriate for the OPTIMADE
+;; .rst file: the Visual Line Mode (the lines are folded dynamically preserving
+;; word boundries) and the (nearly) infinite physical line length
+;; (fill-column).
+;;
+;; S.G 2025-04-22
+;;
 (
 	(nil . (
 		(eval . (visual-line-mode))
