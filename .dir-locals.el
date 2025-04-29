46fefc078d524f4a42521b382a24d04d4acea084
saulius <saulius>
2025-04-29 10:34:54 +0300

    Fixing one more spelling mistake in comments.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;
-;; This file sets editng style for Emacs which is approriate for the OPTIMADE
+;; This file sets editng style for Emacs which is appropriate for the OPTIMADE
 ;; .rst file: the Visual Line Mode (the lines are folded dynamically preserving
 ;; word boundries) and the (nearly) infinite physical line length
 ;; (fill-column).
